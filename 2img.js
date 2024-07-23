3eb4354ba64de214a77ac19d8a61eb5618e45597
PioemFoco <pioemfoco123@gmail.com>
2024-07-23 19:37:17 -0300

    Add files via upload

---
+++ 2img.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.carousel img');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    }
+
+    // Mostra o primeiro slide ao carregar a página
+    showSlide(currentSlide);
+
+    // Avança para o próximo slide a cada 3 segundos (3000 milissegundos)
+    setInterval(nextSlide, 3000);
+   
+});
+
+
